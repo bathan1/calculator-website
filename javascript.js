c6a03685fdcd30233e67f16f0990e857175c460a
Nathan <side_nathan@hotmail.com>
2023-07-09 17:35:46 -0400

    Add operate() function and handle division by 0

--- javascript.js
+++ javascript.js
@@ -11,5 +11,27 @@ function multiply(num1, num2) {
 }
 
 function divide(num1, num2) {
+    if (num2 === 0) {
+        return;
+    }
     return num1 / num2;
+}
+
+function operate(operator, num1, num2) {
+    let result;
+    switch(operator) {
+        case "add":
+            result = add(num1, num2);
+            break;
+        case "subtract":
+            result = subtract(num1, num2);
+            break;
+        case "multiply":
+            result = multiply(num1, num2);
+            break;
+        case "divide":
+            result = divide(num1, num2);
+            break;
+    }
+    return result;
 }
